9154d3d13d0d7de176b34167a9cd9e7b36364fbb
Ethan Sigler <ethansigler@gmail.com>
2018-01-17 16:02:00 +1100

    Updated SoftSpeedController

--- src/org/usfirst/frc/team6035/robot/SoftSpeedController.java
+++ src/org/usfirst/frc/team6035/robot/SoftSpeedController.java
@@ -1,26 +1,51 @@
 package org.usfirst.frc.team6035.robot;
+
 import edu.wpi.first.wpilibj.*;
 
-public class SoftSpeedController {
+public class SoftSpeedController implements SpeedController {
+
+	SpeedController target;
 	
-	Timer timer = new Timer();
-	Joystick stick = new Joystick(0);
-	
-	public void SoftSpeedCont(SpeedControllerGroup motorGroup) {
-		double speedCurr = motorGroup.get();
-		double speedJoy = stick.getY();
-		double speedDiff = speedJoy-speedCurr;
-		double accelTime = 2;
-		
-		while (speedJoy != speedCurr) {
-			motorGroup.set(speedCurr + speedDiff/accelTime);
-			try {
-				timer.wait((long) (speedDiff/accelTime));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	public SoftSpeedController(SpeedController target) {
+	this.target = target;	
 	}
 
+	@Override
+	public void pidWrite(double output) {
+		// TODO Auto-generated method stub
+	target.pidWrite(output);
+		
+	}
+
+	@Override
+	public void set(double speed) {
+		
+	}
+
+	@Override
+	public double get() {
+		// TODO Auto-generated method stub
+		return target.get();
+	}
+
+	@Override
+	public void setInverted(boolean isInverted) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getInverted() {
+		return target.getInverted();
+	}
+
+	@Override
+	public void disable() {
+		target.disable();
+	}
+
+	@Override
+	public void stopMotor() {
+		target.stopMotor();
+	}
 }
