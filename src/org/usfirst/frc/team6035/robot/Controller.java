dcf27ae0033f9acb32b07c28f0ea956d843ecc06
Jek Gum <creepedfire30@gmail.com>
2018-01-18 16:27:27 +1100

    Created Controller, Enabled Robot To Use Controller, Created Grabber GrabberArm And Lift

--- src/org/usfirst/frc/team6035/robot/Controller.java
+++ src/org/usfirst/frc/team6035/robot/Controller.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc.team6035.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class Controller {
+
+	Joystick stick = new Joystick(0);
+	
+	double getDriveSpeed() {
+		
+		return stick.getRawAxis(0);
+	
+	}
+	
+	double getDriveDirection() {
+		
+		return stick.getRawAxis(1);
+	
+	}
 
 }
--- src/org/usfirst/frc/team6035/robot/Robot.java
+++ src/org/usfirst/frc/team6035/robot/Robot.java
@@ -8,12 +8,16 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
      
-	/*
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	Controller controller = new Controller();
+	Lift lift = new Lift();
+	Grabber grabber = new Grabber();
+	GrabberArm grabberArm = new GrabberArm();
+	DriveBase driveBase = new DriveBase();
+	
+	
 	@Override
 	public void robotInit() {
+	
 	}
 
 	public void autonomousInit() {
@@ -28,14 +32,36 @@ public class Robot extends IterativeRobot {
 	public void teleopInit() {
 
 	}
+	
 	@Override
 	public void teleopPeriodic() {
 	
-}
+		driveDriveBase();
+		operateLift();
+		operateGrabberArm();
+		operateGrabber();
+		
+	}
+	
+	private void driveDriveBase() {
+		
+	double speed = controller.getDriveSpeed();
+	double direction = controller.getDriveDirection();
+	driveBase.drive(speed, direction);
+	}
+	
+	private void operateLift() {
+		
+	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
+	private void operateGrabberArm() {
+		
+	}
+	
+	private void operateGrabber() {
+		
+	}
+
 	@Override
 	public void testPeriodic() {
 		//probably not going to be used but may be usefull...
