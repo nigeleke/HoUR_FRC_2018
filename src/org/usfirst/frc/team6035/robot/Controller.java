0241cde0db410f21c740946b2590ffb7bb64de29
Gabriel Ballardie Love <gabrielballardie@gmail.com>
2018-01-22 00:23:25 +0000

    Added methods for returning operations based off controller inputs for game components

--- src/org/usfirst/frc/team6035/robot/Controller.java
+++ src/org/usfirst/frc/team6035/robot/Controller.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team6035.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * @author Jack Gumley
@@ -11,6 +12,7 @@ import edu.wpi.first.wpilibj.Joystick;
 public class Controller {
 
 	Joystick stick = new Joystick(0);
+	XboxController xbox = new XboxController(1);
 	DigitalInput grabberLimitSwitch = new DigitalInput(1);
 	
 	
@@ -31,15 +33,57 @@ public class Controller {
 	
 	}
 	
+	/**
+	 * Return operation for the grabber based off controller input
+	 */
 	GrabberOperation getGrabberOperation() {
-		return GrabberOperation.STOP; // TODO: write this method
+		boolean leftButtonPressed = xbox.getXButtonPressed();
+		boolean rightButtonPressed = xbox.getBButtonPressed();
+		boolean grabberMicroSwitchClosed = grabberLimitSwitch.get();
 		
+		if ((leftButtonPressed) && (!rightButtonPressed)) {
+			if (grabberMicroSwitchClosed = false) {
+				return GrabberOperation.GRAB;
+			}
+			else {
+				return GrabberOperation.HOLD;
+			}
+		}
+		if ((rightButtonPressed) && (!leftButtonPressed)) {
+		return GrabberOperation.LET_GO;
+		}
+		return GrabberOperation.STOP;
 	}
+	/**
+	 * Return operation for grabber arm based off controller input
+	 */
 	GrabberArmOperation getGrabberArmOperation() {
-		return GrabberArmOperation.STOP; // TODO: write this method
+		boolean topButtonPressed = xbox.getYButtonPressed();
+		boolean bottomButtonPressed = xbox.getAButtonPressed();
+		
+		if ((topButtonPressed) && (!bottomButtonPressed)) {
+			return GrabberArmOperation.UP;
+		}
+		if ((bottomButtonPressed) && (!topButtonPressed)) {
+			return GrabberArmOperation.DOWN;
+		}
+		return GrabberArmOperation.STOP; 
 	}
+	/**
+	 * Return operation for lift based off controller input
+	 */
 	LiftOperation getLiftOperation() {
-		return LiftOperation.STOP; // TODO: write this method
+		int dpadVal = xbox.getPOV();
+		boolean goUp = (325 <= dpadVal && dpadVal < 360) || (0 <= dpadVal && dpadVal < 45);
+		boolean goDown = (135 <= dpadVal && dpadVal < 225);
+		
+		if ((goUp) && (!goDown)) {
+			return LiftOperation.UP;
+		}
+		if ((goDown) && (!goUp)) {
+			return LiftOperation.DOWN;
+		}
+		return LiftOperation.STOP; 
 	}
 
 }
