a95eb46780b0355faa1cb53f371e6cbafa04d362
Harrison <Harracule@gmail.com>
2018-02-09 14:16:48 +1100

    added javadoc to autonomous drive

--- src/org/usfirst/frc/team6035/robot/auto/AutoDirection.java
+++ src/org/usfirst/frc/team6035/robot/auto/AutoDirection.java
@@ -1,11 +1,28 @@
 package org.usfirst.frc.team6035.robot.auto;
 
+
+/**
+ * autonomous for base paths
+ * @author SKI
+ *
+ */
 public abstract class AutoDirection {
-
+/**
+ * 
+ * @return number of steps to be executed in the autonomous path
+ */
 	public abstract int nSteps();
-
+/**
+ * 
+ * @param i
+ * @return how fast the drive train runs left at feet per second at the i'th step
+ */
 	public abstract double leftSpeed(int i);
-
+	/**
+	 * 
+	 * @param i
+	 * @return how fast the drive train runs right at feet per second at the i'th step
+	 */
 	public abstract double rightSpeed(int i);
 
 	
