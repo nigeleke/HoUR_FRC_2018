de55785e311073043337168e7b31353e2e7f0667
Gabriel Ballardie Love <gabrielballardie@gmail.com>
2018-02-12 06:22:03 +0000

    turtle steps test

--- design/HoUR_Design/org/usfirst/frc/team6035/robot/controller/RobotPosition.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team6035.robot.controller;
-
-public enum RobotPosition {
-		LEFT, MIDDLE, RIGHT
-}
--- design/HoUR_Design/org/usfirst/frc/team6035/robot/gamecomponents/auto/AutoCommand.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team6035.robot.gamecomponents.auto;
-
-public interface AutoCommand {
-
-	void init();
-	void doNextAction();
-	boolean isFinished();
-	
-}
