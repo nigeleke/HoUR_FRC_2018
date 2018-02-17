d84bcbc0473928739b2ed59f31311442bacbe427
Gabriel Ballardie Love <gabrielballardie@gmail.com>
2018-02-17 01:23:08 +0000

    Added turtle directions for auto directions

--- src/org/usfirst/frc/team6035/robot/auto/LeftToLeftSwitch.java
+++ src/org/usfirst/frc/team6035/robot/auto/LeftToLeftSwitch.java
@@ -3,12 +3,8 @@ package org.usfirst.frc.team6035.robot.auto;
 
 public class LeftToLeftSwitch extends AutoDirection {
 	public LeftToLeftSwitch() {
-		turtle.forward(2.2)
-		.turn(-25)
-		.forward(2)
-		.turn(45)
-		.forward(1)
+		turtle.forward(4.2)
 		.turn(90)
-		.forward(0.1);
+		.forward(0.8);
 	}
 }
