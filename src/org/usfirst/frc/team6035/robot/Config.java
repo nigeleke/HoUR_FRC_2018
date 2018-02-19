a55ff635f8b279533282c61ceaebd717b576e9a0
Jek Gum <creepedfire30@gmail.com>
2018-02-19 19:27:36 +1100

    Fixed Incorrect Ports

--- src/org/usfirst/frc/team6035/robot/Config.java
+++ src/org/usfirst/frc/team6035/robot/Config.java
@@ -25,9 +25,9 @@ public class Config {
 	public static int GRABBER_CHANNEL = 7 ;
 	public static int GRABBER_ARM_CHANNEL = 4;
 	public static int LIFT_CHANNEL_1 = 6;
-	public static int LIFT_CHANNEL_2 = 9;
+	public static int LIFT_CHANNEL_2 = 8;
 	public static int CLIMBER_CHANNEL = 5;
-	public static int PUSHER_CHANNEL = 8;
+	public static int PUSHER_CHANNEL = 9;
 	// ...
 	public static int GRABBER_SWITCH_CHANNEL_DIO = 7;
 	public static int LIFT_UP_TRAVEL_DIO = 0;
