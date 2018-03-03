00a62c9e4c62410f00369e99a3e3e77758fc2d6d
Nigel Eke <me@nigel-eke.com>
2018-03-03 11:17:00 +1100

    Added back TestResource main - for Autonomous testing with resources

---
+++ src/org/usfirst/frc/team6035/robot/auto/recorded/TestResource.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team6035.robot.auto.recorded;
+
+import org.usfirst.frc.team6035.robot.auto.AutoPlay;
+
+public class TestResource extends AutoPlay {
+
+	public TestResource() {
+		super("LeftToLeftSwitch.dat", true);
+	}
+
+	public static void main(String[] args) {
+		TestResource tr = new TestResource();
+		System.out.println(tr);
+	}
+
+}
